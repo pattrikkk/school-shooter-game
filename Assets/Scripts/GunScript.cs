7f030fca8251e580d236dd3bbd74e8f9c69d6060
kolibrik666 <58887215+kolibrik666@users.noreply.github.com>
2025-04-26 16:59:04 +0200

    Update GunScript.cs

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class GunScript : MonoBehaviour
 {
     public GameObject projectile;
-    public float power = 10.0f;
+    public float power = 20.0f;
     // sila/rýchlosť výstrelu    
     public GameObject shootPoint;
     // pozícia na ktorej vznikne projektil  
@@ -13,5 +13,22 @@ public class GunScript : MonoBehaviour
     {
         GameObject newProjectile = Instantiate(projectile, shootPoint.transform.position, shootPoint.transform.rotation) as GameObject;
         newProjectile.GetComponent<Rigidbody>().AddForce(grabPoint.transform.forward * power, ForceMode.VelocityChange);
+    }
+
+    private void ShootRay()
+    {
+        Ray ray = new Ray(shootPoint.transform.position, shootPoint.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 20f))
+        {
+            Debug.Log("Hit: " + hit.collider.name);
+
+            if (hit.collider.CompareTag("Enemy"))
+            {
+
+                hit.collider.GetComponentInParent<EnemyAI>().TakeDamage(1);
+            }
+        }
     }
 }
